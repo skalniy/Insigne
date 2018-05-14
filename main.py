f8dd0fb79ece9488a756825dad072ec2cbec960a
skalniy <skalniy@inbox.ru>
2018-05-14 13:57:57 +0300

    PoC

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pdf_file import PDFFile
 def main():
     pdf = PDFFile.open('./testPDF_Version.6.x.pdf')
     pdf.sign('./key1.txt', './key2.txt')
+    pdf.sign('./key1.txt', './key2.txt')
+    pdf.sign('./key1.txt', './key2.txt')
     pdf.save('./signed.pdf')
     pdf2 = PDFFile.open('./signed.pdf')
     print(pdf2.validate())
